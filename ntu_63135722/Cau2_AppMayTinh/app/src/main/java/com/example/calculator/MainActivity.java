220852b9645f6f9d62e9e0030ae41e7e24d4eca5
vntrungtin1909 <91836645+vntrungtin1909@users.noreply.github.com>
2024-03-25 16:37:15 +0700

    tạo các biến xử lí backend
    tạo các biến số, chức năng

--- ntu_63135722/Cau2_AppMayTinh/app/src/main/java/com/example/calculator/MainActivity.java
+++ ntu_63135722/Cau2_AppMayTinh/app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.calculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,54 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
+
 public class MainActivity extends AppCompatActivity {
+    double first;
+    String operation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        Button num0 = findViewById(R.id.num0);
+        Button num1 = findViewById(R.id.num1);
+        Button num2 = findViewById(R.id.num2);
+        Button num3 = findViewById(R.id.num3);
+        Button num4 = findViewById(R.id.num4);
+        Button num5 = findViewById(R.id.num5);
+        Button num6 = findViewById(R.id.num6);
+        Button num7 = findViewById(R.id.num7);
+        Button num8 = findViewById(R.id.num8);
+        Button num9 = findViewById(R.id.num9);
+
+        Button on = findViewById(R.id.on);
+        Button off = findViewById(R.id.off);
+        Button ac = findViewById(R.id.ac);
+        Button del = findViewById(R.id.del);
+        Button chia = findViewById(R.id.chia);
+        Button nhan = findViewById(R.id.nhan);
+        Button tru = findViewById(R.id.tru);
+        Button cong = findViewById(R.id.cong);
+        Button equal = findViewById(R.id.equal);
+        Button point = findViewById(R.id.point);
+
+        TextView screen = findViewById(R.id.screen);
+
+        ArrayList<Button> nums = new ArrayList<>();
+        nums.add(num0);
+        nums.add(num1);
+        nums.add(num2);
+        nums.add(num3);
+        nums.add(num4);
+        nums.add(num5);
+        nums.add(num6);
+        nums.add(num7);
+        nums.add(num8);
+        nums.add(num9);
+
     }
 }
