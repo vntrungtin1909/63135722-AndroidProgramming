d5469408ab6d52ad4c5ce315e177694e2a342074
vntrungtin1909 <91836645+vntrungtin1909@users.noreply.github.com>
2024-04-16 16:47:48 +0700

    làm tiếp view page

---
+++ ViewPaper2RecyclerViewAdapter/app/src/main/java/com/example/viewpage2/LandScapeAdapter.java
@@ -0,0 +1,78 @@
+package com.example.viewpage2;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class LandScapeAdapter extends RecyclerView.Adapter<LandScapeAdapter.ItemLandHolder> {
+    Context context;
+    ArrayList<LandScape> listData;
+
+    public LandScapeAdapter(Context context, ArrayList<LandScape> listData) {
+        this.context = context;
+        this.listData = listData;
+    }
+
+    @NonNull
+    @Override
+    public ItemLandHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater cai_bom = LayoutInflater.from(context);
+        View viewItem = cai_bom.inflate(R.layout.item_land, parent, false);
+
+        ItemLandHolder holderCreated = new ItemLandHolder(viewItem);
+        return holderCreated;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemLandHolder holder, int position) {
+        // Lấy đối tượng hiển thị
+        LandScape landScapeHienThi = listData.get(position);
+        // Lấy thông tin
+        String caption = landScapeHienThi.getLandCaption();
+        String tenFileAnh = landScapeHienThi.getLandImageFileName();
+        // Đặt vào các trường thông tin của holder
+        holder.tvCaption.setText(caption);
+        // Đặt ảnh
+        String packageName = holder.itemView.getContext().getPackageName();
+        int imageId = holder.itemView.getResources().getIdentifier(tenFileAnh, "mipmap", packageName);
+        holder.ivLandScape.setImageResource(imageId);
+    }
+
+    @Override
+    public int getItemCount() {
+        return listData.size();
+    }
+
+    class ItemLandHolder extends RecyclerView.ViewHolder implements  View.OnClickListener{
+        TextView tvCaption;
+        ImageView ivLandScape;
+
+        public ItemLandHolder(@NonNull View itemView) {
+            super(itemView);
+            tvCaption = itemView.findViewById(R.id.textViewCaption);
+            ivLandScape = itemView.findViewById(R.id.imageViewLand);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            int viTriDuocClick = getAdapterPosition();
+            LandScape phanTuDuocClick = listData.get(viTriDuocClick);
+            // Lấy thông tin
+            String ten = phanTuDuocClick.getLandCaption();
+            String tenFileAnh = phanTuDuocClick.getLandImageFileName();
+            // Toast Tên
+            String chuoiThongBao = "Bạn vừa click vào: " + ten;
+            Toast.makeText(view.getContext(), chuoiThongBao, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
---
+++ ViewPaper2RecyclerViewAdapter/app/src/main/java/com/example/viewpage2/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.viewpage2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    LandScapeAdapter landScapeAdapter;
+    ArrayList<LandScape> viewPagerData;
+
+    ViewPager2 viewPager2Land;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewPagerData = getDataForViewPager();
+        viewPager2Land = findViewById(R.id.vp2Land);
+        landScapeAdapter = new LandScapeAdapter(this, viewPagerData);
+        viewPager2Land.setAdapter(landScapeAdapter);
+        viewPager2Land.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                super.onPageScrollStateChanged(state);
+            }
+        });
+    }
+
+    ArrayList<LandScape> getDataForViewPager() {
+        ArrayList<LandScape> dsDuLieu = new ArrayList<>();
+        dsDuLieu.add(new LandScape("thaphanoi", "Cột cờ Hà Nội"));
+        dsDuLieu.add(new LandScape("effel", "Tháp Effel"));
+        dsDuLieu.add(new LandScape("buckingham", "Cung điện Buckinghham"));
+        dsDuLieu.add(new LandScape("thaphanoi", "Cột cờ Hà Nội"));
+        dsDuLieu.add(new LandScape("effel", "Tháp Effel"));
+        dsDuLieu.add(new LandScape("buckingham", "Cung điện Buckinghham"));
+        return dsDuLieu;
+    }
+}
