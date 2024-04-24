648574582c5b009f499a46237d3ab556bab323aa
vntrungtin1909 <91836645+vntrungtin1909@users.noreply.github.com>
2024-04-24 21:20:08 +0700

    làm ví dụ intent

---
+++ ExplicitIntent/app/src/main/java/com/example/explicitintent/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.explicitintent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void ChuyenManHinh(View v){
+        Intent iManHinhKhac = new Intent(this, SubActivityOne.class);
+        startActivity(iManHinhKhac);
+    }
+}
---
+++ ExplicitIntent/app/src/main/java/com/example/explicitintent/SubActivityOne.java
@@ -0,0 +1,25 @@
+package com.example.explicitintent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SubActivityOne extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub_one);
+    }
+
+    public void QuayVe(View v){
+        Intent iManHinhChinh = new Intent(this, MainActivity.class);
+        startActivity(iManHinhChinh);
+    }
+}
